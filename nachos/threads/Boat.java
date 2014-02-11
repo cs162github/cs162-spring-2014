1c07f81d04e1b8b6926a16e3052649ca8b21ff43
Isaac Tian <itian.calTA@gmail.com>
2014-02-10 16:53:37 -0800

    Changes made for Spring 2014
    Boat.java, BoatGrader.java changed to comply with new autograder tests

--- nachos/threads/Boat.java
+++ nachos/threads/Boat.java
@@ -43,6 +43,9 @@ public class Boat
 
     static void AdultItinerary()
     {
+	bg.initializeAdult(); //Required for autograder interface. Must be the first thing called.
+	//DO NOT PUT ANYTHING ABOVE THIS LINE. 
+
 	/* This is where you should put your solutions. Make calls
 	   to the BoatGrader to show that it is synchronized. For
 	   example:
@@ -53,6 +56,8 @@ public class Boat
 
     static void ChildItinerary()
     {
+	bg.initializeChild(); //Required for autograder interface. Must be the first thing called.
+	//DO NOT PUT ANYTHING ABOVE THIS LINE. 
     }
 
     static void SampleItinerary()
